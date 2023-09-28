f62e203efab843f2c45915f406c3bdc11fd2da0a
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2023-09-28 11:28:34 +0300

    add card render function and styles

---
+++ src/js/utilities/render-gallery.js
@@ -0,0 +1,31 @@
+export function createCocktailCards(arr, container) {
+  const markup = arr
+    .map(
+      item => `<li class="cocktail-card" data-id="${item._id}">
+      <div class="cocktail-img-wrap">
+        <img
+          class="cocktail-img"
+          src="${item.drinkThumb}"
+          alt="${item.drink}"
+          loading="lazy"
+        />
+      </div>
+      <div class="cocktail-info">
+        <h3 class="cocktail-name">${item.drink}</h3>
+        <p class="cocktail-description">${item.description}</p>
+        <div class="cic-btn-wrapper">
+          <button class="cocktail-learn-more-btn">learn more</button>
+          <button class="cocktail-add-fav-btn" data-type="user-action" data-action="addtofav">
+            <svg
+              class="favorites-btn-icon"
+                href="../img/icons.svg#icon-heart"
+              ></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </li>`
+    )
+    .join('');
+  container.innerHTML = markup;
+}
