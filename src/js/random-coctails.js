3651d71df097e0fbf0b5ba66044e4edaa3f04b7e
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-09-29 22:53:11 +0300

    randomgalery

--- src/js/random-coctails.js
+++ src/js/random-coctails.js
@@ -1,11 +1,13 @@
 import {fetchRandomCocktails} from '../js/utilities/fetch-data'
 import { createCocktailCards } from "./utilities/render-gallery";
 const randomGallery = document.querySelector('.gallery-list')
-
-if(window.innerWidth>=1280){ 
-  let render = await fetchRandomCocktails(9);
-  createCocktailCards(render, randomGallery )
-}else {
-  let render = await fetchRandomCocktails(8);
-  createCocktailCards(render, randomGallery )
+const rondomGallery = async() => {
+  if(window.innerWidth>=1280){ 
+    let render = await fetchRandomCocktails(9);
+    createCocktailCards(render, randomGallery )
+  }else {
+    let render = await fetchRandomCocktails(8);
+    createCocktailCards(render, randomGallery )
+  }
 }
+rondomGallery()
