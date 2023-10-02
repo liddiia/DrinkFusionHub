33e0acc73ec8f5de63ebd281fa30b46a6fe9a46b
liddiia <liddabojko@gmail.com>
2023-10-03 02:25:46 +0300

    favorit ingrid and render fav ingr add

--- src/js/favorite-ingridients.js
+++ src/js/favorite-ingridients.js
@@ -1,7 +1,18 @@
 import './mob-menu';
+//import './modal';
+//import './modal-coctail';
+//import './modal-igridients';
+import { ingredients } from './utilities/local-storage'; 
+import haventAddedFavoriteIngradients from "./utilities/no-cocktails";
 
-import './modal';
-import './modal-coctail';
-import './modal-igridients';
+const contNoCoctails = document.querySelector('container-non-photos');
+const titleGalleryEl = document.querySelector('gallery-header');
+const gelleryListEl = document.querySelector('gallery-list');
 
-console.log('hello world');
+if (ingredients.lenght===0 || !ingredients) {
+haventAddedFavoriteIngradients(contNoCoctails,titleGalleryEl);
+}
+else {
+console.log( ingredients );
+}
+//renderFavIngidients
