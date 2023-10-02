95a2aa64da5e8ce6fff4de3b82b6d98b5dc3a7b3
liddiia <liddabojko@gmail.com>
2023-10-03 02:37:18 +0300

    Update favorite-ingridients.js

--- src/js/favorite-ingridients.js
+++ src/js/favorite-ingridients.js
@@ -1,18 +1,1 @@
-import './mob-menu';
-//import './modal';
-//import './modal-coctail';
-//import './modal-igridients';
-import { ingredients } from './utilities/local-storage'; 
-import haventAddedFavoriteIngradients from "./utilities/no-cocktails";
 
-const contNoCoctails = document.querySelector('container-non-photos');
-const titleGalleryEl = document.querySelector('gallery-header');
-const gelleryListEl = document.querySelector('gallery-list');
-
-if (ingredients.lenght===0 || !ingredients) {
-haventAddedFavoriteIngradients(contNoCoctails,titleGalleryEl);
-}
-else {
-console.log( ingredients );
-}
-//renderFavIngidients
