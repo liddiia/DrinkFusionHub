43df79d486cec3c4319afee2a0d251547dd479a2
liddiia <liddabojko@gmail.com>
2023-10-02 22:59:54 +0300

    commit html

--- src/js/favorite-cocktails.js
+++ src/js/favorite-cocktails.js
@@ -3,3 +3,19 @@ import './favorite'
 import './modal'
 import './modal-coctail'
 import './modal-igridients'
+
+console.log("hello");
+
+//import {fetchRandomCocktails} from '../js/utilities/fetch-data'
+//import { createCocktailCards } from "./utilities/render-gallery";
+//const randomGallery = document.querySelector('.gallery-list')
+//const rondomGallery = async() => {
+//   if(window.innerWidth>=1280){ 
+//     let render = await fetchRandomCocktails(9);
+//     createCocktailCards(render, randomGallery )
+//   }else {
+//     let render = await fetchRandomCocktails(8);
+//     createCocktailCards(render, randomGallery )
+//   }
+// }
+// rondomGallery()
