7d9f784d17a3406ce178f86f45faee135578c646
liddiia <127796774+liddiia@users.noreply.github.com>
2023-10-03 12:19:08 +0000

    Deploying to gh-pages from @ liddiia/DrinkFusionHub@993f2a3168512db4702de08caa12b2090f6d23e8 🚀

--- assets/favorite-cocktails-362a973f.js
+++ assets/favorite-cocktails-362a973f.js
@@ -1,4 +1,4 @@
-import{d as r,C as s,f as a,h as l}from"./no-cocktails-58ff2382.js";import{g as n}from"./modalCallDrink-c24e8f6e.js";function d(t,e){const c=t.map(o=>`<li class="cocktail-card" data-id="${o._id}">
+import{d as r,C as s,f as a,h as l}from"./no-cocktails-58ff2382.js";import{g as n}from"./modalCallDrink-024008a8.js";function d(t,e){const c=t.map(o=>`<li class="cocktail-card" data-id="${o._id}">
       <div class="cocktail-img-wrap">
         <img
           class="cocktail-img"
