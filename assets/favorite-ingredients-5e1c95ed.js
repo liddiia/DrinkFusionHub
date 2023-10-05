957f684e80c980dc0c923ff37cd68ae9eadc1de3
BielovaIryna <130380331+BielovaIryna@users.noreply.github.com>
2023-10-05 23:57:29 +0000

    Deploying to gh-pages from @ liddiia/DrinkFusionHub@b635ed22a596fb6d4c79df9ccff2d56e3751df57 🚀

--- assets/favorite-ingredients-5e1c95ed.js
+++ assets/favorite-ingredients-5e1c95ed.js
@@ -1,4 +1,4 @@
-import{r as b,s as w,m as k,d as F,a as G,l as h,e as R,g as U,n as V,I as W,b as j}from"./icons-e7b7e346.js";function B(a){b.favoriteIngPaginationContainer.innerHTML=`<button
+import{r as b,s as w,m as k,d as F,a as G,l as h,e as R,g as U,n as V,I as W,b as j}from"./icons-d3163b46.js";function B(a){b.favoriteIngPaginationContainer.innerHTML=`<button
   class="left-pag-btn pagination-button-item"
   type="button"
   id="left-pag-btn"
