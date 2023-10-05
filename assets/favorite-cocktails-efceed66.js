f3b58464c7a3685081867a11a1d1714e97ab7fc9
liddiia <127796774+liddiia@users.noreply.github.com>
2023-10-05 14:14:10 +0000

    Deploying to gh-pages from @ liddiia/DrinkFusionHub@2a465779532e210ba48f4ed8f1a818c98d978ce1 🚀

--- assets/favorite-cocktails-efceed66.js
+++ assets/favorite-cocktails-efceed66.js
@@ -1,4 +1,4 @@
-import{r as l,s as w,f as A,m as I,g as q,a as F}from"./icons-45cf906d.js";import"./modal-coctail-c5c16bb5.js";function S(d){l.favoritePaginationContainer.innerHTML=`<button
+import{r as l,s as w,f as A,m as I,g as q,a as F}from"./icons-944421b6.js";import"./modal-coctail-cd68825f.js";function S(d){l.favoritePaginationContainer.innerHTML=`<button
   class="left-pag-btn pagination-button-item"
   type="button"
   id="left-pag-btn"
