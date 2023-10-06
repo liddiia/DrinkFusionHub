1f00c149ca1880ae0a60bbdd7b2ea79dc5e44d5e
liddiia <127796774+liddiia@users.noreply.github.com>
2023-10-06 00:27:25 +0000

    Deploying to gh-pages from @ liddiia/DrinkFusionHub@45d0c9a489b65e89b1f26f7d07470481b4120f59 🚀

---
+++ assets/favorite-cocktails-0d955913.js
@@ -0,0 +1,65 @@
+import{r as u,s as w,m as h,d as F,C as q,a as O,b as j,c as z,f as k,e as K,g as U,h as V,i as W,j as G}from"./icons-5bc4a62a.js";import"./modal-coctail-013cd45c.js";function B(i){u.favoritePaginationContainer.innerHTML=`<button
+  class="left-pag-btn pagination-button-item"
+  type="button"
+  id="left-pag-btn"
+  data-action="leftPag"
+>
+<svg
+class="page-left-icon-container"
+id="left-pag-btn-svg"
+data-action="leftPag"
+>
+<use
+  class="page-left-icon"
+  href="${w}#icon-arrow_left"
+  data-action="leftPag"
+></use>
+</svg>
+</button>
+<div class="number-buttons-container"></div>
+<button
+  class="right-pag-btn pagination-button-item"
+  type="button"
+  id="right-pag-btn"
+  data-action="rightPag"
+>
+<svg
+class="page-right-icon-container"
+id="right-pag-btn-svg"
+data-action="rightPag"
+>
+<use
+  class="page-right-icon"
+  href="${w}#icon-arrow_right"
+  data-action="rightPag"
+></use>
+</svg>
+</button>`;const a=document.querySelector(".number-buttons-container"),f=document.getElementById("left-pag-btn"),s=document.getElementById("right-pag-btn");a.innerHTML="";let d=6,t=0,g=null,e=[],r=[],m=[],C=[],v=[],p=[],P=[],n,b;function M(){return!(window.innerWidth>=768)}let A=i.length;A<=d?u.favoritePaginationContainer.classList.add("is-hidden"):u.favoritePaginationContainer.classList.remove("is-hidden");let c=Math.ceil(A/d);const I=N(i,d);T(f,s);function N(l,o){return l.reduce((L,_,x)=>(x%o===0&&L.push([]),L[L.length-1].push(_),L),[])}H(I[0]),f.addEventListener("click",E),s.addEventListener("click",E),a.addEventListener("click",E);function D(){g&&g.classList.remove("pagination-button-item-active"),g=e[t],g.classList.add("pagination-button-item-active")}function S(){switch(a.innerHTML="",r=e.slice(0,3),m=e.slice(0,1),C=e.slice(0,4),v=e.slice(e.length-3,e.length),p=e.slice(e.length-1,e.length),P=e.slice(e.length-4,e.length),a.innerHTML="",n=document.createElement("button"),n.textContent="...",n.setAttribute("disabled",""),n.classList.add("pagination-button-item"),b=document.createElement("button"),b.textContent="...",b.setAttribute("disabled",""),b.setAttribute("data-disable","true"),b.classList.add("pagination-button-item"),M()){case!0:{e.length<=5?a.append(...e):t===0||t===1?a.append(...r,n,...p):t===2?a.append(...C,n,...p):t===c-3?a.append(...m,n,...P):t===c-2||t===c-1?a.append(...m,n,...v):t>=3&&t<=c-4&&a.append(...m,n,b,e[t],n,...p);break}case!1:{e.length<=7?a.append(...e):t===0||t===1?a.append(...r,n,...v):t===2?a.append(...C,n,...p):t===c-3?a.append(...m,n,...P):t===c-2||t===c-1?a.append(...r,n,...v):t>=3&&t<=c-4&&a.append(...m,n,b,...e.slice(t-1,t+2),n,...p);break}}}function E(l){y.classList.remove("pagination-button-item-active");let o=l.target.dataset.action;switch(u.favoritesList.innerHTML="",o){case"leftPag":t-=1,t<0&&(t=0);break;case"rightPag":t+=1,t>=c&&(t=c-1);break;default:isNaN(Number(o))||(t=Number(o-1));break}H(I[t]),S(),D(),T(f,s)}function T(l,o){t===0?l.classList.add("is-hidden"):l.classList.remove("is-hidden"),t===c-1?o.classList.add("is-hidden"):o.classList.remove("is-hidden")}for(let l=1;l<=c;l++){const o=document.createElement("button");o.textContent=l,o.setAttribute("data-action",l),o.classList.add("pagination-button-item"),e.push(o)}switch(r=e.slice(0,3),v=e.slice(e.length-3,e.length),p=e.slice(e.length-1,e.length),n=document.createElement("button"),n.textContent="...",n.setAttribute("disabled",""),n.classList.add("pagination-button-item"),a.innerHTML="",M()){case!0:{e.length<=5?a.append(...e):(t===0||t===1)&&a.append(...r,n,...p);break}case!1:{e.length<=7?a.append(...e):(t===0||t===1)&&a.append(...r,n,...v);break}}const y=document.querySelector('button[data-action="1"]');y&&y.classList.add("pagination-button-item-active")}const $=i=>{h.classList.toggle("is-open"),K(i)};function J(i,a){const f=d=>{const{target:t}=d,g=t.dataset.idDrink;t.classList.contains("cocktail-learn-more-btn")&&(setTimeout(h.classList.toggle("is-open"),500),a(g),F(d),localStorage.setItem(q,g));const e=r=>{r.key==="Escape"&&($(r),document.removeEventListener("keydown",e))};document.addEventListener("keydown",e)},s=d=>{const{target:t}=d;if(t===O||t===h||t.classList.contains("cocktail-modal-back")||t.tagName==="A")if(t.tagName==="A"&&t.classList.contains("ingredient-link")){d.preventDefault();const g=t.dataset.idIngridient;j(g),h.addEventListener("click",z);return}else $(t),B(k)};i.addEventListener("click",f),h.addEventListener("click",s)}const Q=document.querySelector(".favorite-container-nococktails"),H=i=>{u.favoritesList.innerHTML="",!i||i.length===0?Q.classList.add("active"):i.forEach(async a=>{const f=await V(a);if(!f[0])return;const s=f[0];u.favoritesList.innerHTML+=`<li class="cocktail-card" data-id="${s._id}">
+      <div class="cocktail-img-wrap">
+        <img
+          class="cocktail-img"
+          src="${s.drinkThumb}"
+          onerror = "this.onerror=null;this.src='/img/no-coctails/no-coktails@1x.png';"
+          alt="${s.drink}"
+          loading="lazy"
+        />
+      </div>
+      <div class="cocktail-info">
+        <h3 class="cocktail-name">${s.drink}</h3>
+        <p class="cocktail-description">${s.description}</p>
+        <div class="cic-btn-wrapper">
+          <button class="cocktail-learn-more-btn" data-id-drink="${s._id}">learn more</button>
+          <button class="cocktail-rem-fav-btn remove-from-fav-btn" data-type="user-action" id ="${s._id}" data-action="remfromfav">
+            <svg
+              class="remove-favorites-btn-icon"
+              aria-label="remove from favorites button"
+            >
+              <use
+                class="remove-icon"
+                href="${w}#icon-remove"
+              ></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </li>`})};B(k);u.favoritesList&&J(u.favoritesList,G);u.favoritesList.addEventListener("click",i=>{if(i.target.classList.contains("remove-from-fav-btn")){const a=i.target.getAttribute("id");U(a,k,W),B(k)}});
---
+++ assets/favorite-ingredients-f6023692.js
@@ -0,0 +1,61 @@
+import{r as p,s as C,m as L,d as F,a as G,l as I,e as R,g as U,n as V,I as W,b as j}from"./icons-5bc4a62a.js";function w(a){p.favoriteIngPaginationContainer.innerHTML=`<button
+  class="left-pag-btn pagination-button-item"
+  type="button"
+  id="left-pag-btn"
+  data-action="leftPag"
+>
+<svg
+class="page-left-icon-container"
+id="left-pag-btn-svg"
+data-action="leftPag"
+>
+<use
+  class="page-left-icon"
+  href="${C}#icon-arrow_left"
+  data-action="leftPag"
+></use>
+</svg>
+</button>
+<div class="number-buttons-container"></div>
+<button
+  class="right-pag-btn pagination-button-item"
+  type="button"
+  id="right-pag-btn"
+  data-action="rightPag"
+>
+<svg
+class="page-right-icon-container"
+id="right-pag-btn-svg"
+data-action="rightPag"
+>
+<use
+  class="page-right-icon"
+  href="${C}#icon-arrow_right"
+  data-action="rightPag"
+></use>
+</svg>
+</button>`;const n=document.querySelector(".number-buttons-container"),d=document.getElementById("left-pag-btn"),s=document.getElementById("right-pag-btn");n.innerHTML="";let g=6,t=0,b=null,e=[],l=[],m=[],k=[],v=[],u=[],y=[],i,f;function B(){return!(window.innerWidth>=768)}let M=a.length;M<=g?p.favoriteIngPaginationContainer.classList.add("is-hidden"):p.favoriteIngPaginationContainer.classList.remove("is-hidden");let r=Math.ceil(M/g);const T=S(a,g);$(d,s);function S(c,o){return c.reduce((h,q,D)=>(D%o===0&&h.push([]),h[h.length-1].push(q),h),[])}A(T[0]),d.addEventListener("click",E),s.addEventListener("click",E),n.addEventListener("click",E);function _(){b&&b.classList.remove("pagination-button-item-active"),b=e[t],b.classList.add("pagination-button-item-active")}function x(){switch(n.innerHTML="",l=e.slice(0,3),m=e.slice(0,1),k=e.slice(0,4),v=e.slice(e.length-3,e.length),u=e.slice(e.length-1,e.length),y=e.slice(e.length-4,e.length),n.innerHTML="",i=document.createElement("button"),i.textContent="...",i.setAttribute("disabled",""),i.classList.add("pagination-button-item"),f=document.createElement("button"),f.textContent="...",f.setAttribute("disabled",""),f.setAttribute("data-disable","true"),f.classList.add("pagination-button-item"),B()){case!0:{e.length<=5?n.append(...e):t===0||t===1?n.append(...l,i,...u):t===2?n.append(...k,i,...u):t===r-3?n.append(...m,i,...y):t===r-2||t===r-1?n.append(...m,i,...v):t>=3&&t<=r-4&&n.append(...m,i,f,e[t],i,...u);break}case!1:{e.length<=7?n.append(...e):t===0||t===1?n.append(...l,i,...v):t===2?n.append(...k,i,...u):t===r-3?n.append(...m,i,...y):t===r-2||t===r-1?n.append(...l,i,...v):t>=3&&t<=r-4&&n.append(...m,i,f,...e.slice(t-1,t+2),i,...u);break}}}function E(c){P.classList.remove("pagination-button-item-active");let o=c.target.dataset.action;switch(p.favIngredientsList.innerHTML="",o){case"leftPag":t-=1,t<0&&(t=0);break;case"rightPag":t+=1,t>=r&&(t=r-1);break;default:isNaN(Number(o))||(t=Number(o-1));break}A(T[t]),x(),_(),$(d,s)}function $(c,o){t===0?c.classList.add("is-hidden"):c.classList.remove("is-hidden"),t===r-1?o.classList.add("is-hidden"):o.classList.remove("is-hidden")}for(let c=1;c<=r;c++){const o=document.createElement("button");o.textContent=c,o.setAttribute("data-action",c),o.classList.add("pagination-button-item"),e.push(o)}switch(l=e.slice(0,3),v=e.slice(e.length-3,e.length),u=e.slice(e.length-1,e.length),i=document.createElement("button"),i.textContent="...",i.setAttribute("disabled",""),i.classList.add("pagination-button-item"),n.innerHTML="",B()){case!0:{e.length<=5?n.append(...e):(t===0||t===1)&&n.append(...l,i,...u);break}case!1:{e.length<=7?n.append(...e):(t===0||t===1)&&n.append(...l,i,...v);break}}const P=document.querySelector('button[data-action="1"]');P&&P.classList.add("pagination-button-item-active")}const H=a=>{L.classList.toggle("is-open"),R(a)};function z(a,n){const d=g=>{const{target:t}=g,b=t.dataset.idIgridient;t.classList.contains("cocktail-learn-more-btn")&&(setTimeout(L.classList.toggle("is-open"),500),n(b),F(g));const e=l=>{l.key==="Escape"&&(H(l),document.removeEventListener("keydown",e))};document.addEventListener("keydown",e)},s=g=>{const{target:t}=g;(t===G||t===L||t.classList.contains("ingridient-modal-back-btn"))&&(H(t),w(I))};a.addEventListener("click",d),L.addEventListener("click",s)}const J=document.querySelector(".container-non-photos"),N=document.querySelector(".fav-ingredients-cocktails-list"),A=a=>{p.favIngredientsList.innerHTML="",!a||a.length===0?J.classList.add("active"):a.forEach(async n=>{const d=await V(n);if(!d[0])return;const s=d[0];p.favIngredientsList.innerHTML+=` <li class= "fav-ingr-item">
+      <h2 class="fav-ingridient-title" data-id-igridient =${s._id}>${s.title}</h2>
+
+  <p class="fav-ingridient-type" >${s.type}</p>
+
+  <p class="fav-ingridient-discription"><span class="ingr-title">${s.title}: </span>${K(s.description)}</p>
+
+
+  <div class="cic-btn-wrapper">
+  <button class="cocktail-learn-more-btn" data-id-igridient="${s._id}">learn more
+  </button>
+
+  <button class="cocktail-rem-fav-btn fav-ingred-remove-button" id ="${s._id}" data-type="user-action" data-action="remfromfav">
+  <svg
+  class="remove-favorites-btn-icon"
+  aria-label="remove from favorites button"
+  >
+  <use
+  class="remove-icon"
+  href="${C}#icon-remove"
+  ></use>
+  </svg>
+  </button>
+  </div>
+  </li>`})};function K(a){return a||"Sorry we dont have it yet"}w(I);N&&z(N,j);p.favIngredientsList.addEventListener("click",a=>{if(a.target.classList.contains("fav-ingred-remove-button")){const n=a.target.getAttribute("id");U(n,I,W),w(I)}});
