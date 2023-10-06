897cf44ca912f12aa757d559d1e3a576df48dd5f
Dmytro Sayko <saykodmytro@icloud.com>
2023-10-06 18:15:11 +0300

    update main page

--- src/js/random-coctails.js
+++ src/js/random-coctails.js
@@ -1,19 +1,18 @@
-import {fetchRandomCocktails} from '../js/utilities/fetch-data'
-import { createCocktailCards } from "./utilities/render-gallery";
+import { fetchRandomCocktails } from '../js/utilities/fetch-data';
+import { createCocktailCards } from './utilities/render-gallery';
 import { refs } from './utilities/refs';
 const randomGallery = document.querySelector('.gallery-list');
-const rondomGallery = async() => {
-  if(window.innerWidth>=1280){
+const rondomGallery = async () => {
+  if (window.innerWidth >= 1280) {
     let render = await fetchRandomCocktails(9);
-    createCocktailCards(render, randomGallery )
-  }else {
+    createCocktailCards(render, randomGallery);
+  } else {
     let render = await fetchRandomCocktails(8);
-    createCocktailCards(render, randomGallery )
+    createCocktailCards(render, randomGallery);
   }
   refs.paginationContainer.classList.add('is-hidden');
-}
-rondomGallery()
+};
+rondomGallery();
 
-const toSearch = document.getElementById('search');
-toSearch.scrollIntoView({behavior: 'smooth'}, true)
-
+// const toSearch = document.getElementById('search');
+// toSearch.scrollIntoView({behavior: 'smooth'}, true)
